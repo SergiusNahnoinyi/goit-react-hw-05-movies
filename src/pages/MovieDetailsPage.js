dad8c450887ac6b6a386a9e7f3126b4b0cd0a48e
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-03-17 15:33:58 +0200

    Стилизирует карточку фильма

--- src/pages/MovieDetailsPage.js
+++ src/pages/MovieDetailsPage.js
@@ -1,55 +1,16 @@
 import { useState, useEffect } from 'react';
-import { useParams, Link, Outlet, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
+import GalleryItem from '../components/GalleryItem/GalleryItem';
 import moviesApi from '../services/api-service';
 
 export default function MovieDetailsPage() {
   const [movie, setMovie] = useState(null);
   const { movieId } = useParams();
-  let navigate = useNavigate();
 
   useEffect(() => {
     moviesApi.getMovieById(movieId).then(setMovie);
   }, [movieId]);
 
-  return (
-    <>
-      <button
-        type="button"
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        Go back
-      </button>
-      {movie && (
-        <div style={{ display: 'flex', marginTop: '16px' }}>
-          <img
-            src={`https://image.tmdb.org/t/p/w300` + movie.poster_path}
-            alt={movie.title}
-          />
-          <div style={{ marginLeft: '20px' }}>
-            <h1>{movie.title}</h1>
-            <p>User Score: {movie.vote_average * 10}%</p>
-            <h2>Overview</h2>
-            <p>{movie.overview}</p>
-            <h3>Genres</h3>
-            <p>{movie.genres.map(genre => genre.name).join(' ')}</p>
-          </div>
-        </div>
-      )}
-      <hr />
-      <h4>Additional information</h4>
-      <ul>
-        <li>
-          <Link to="cast">Cast</Link>
-        </li>
-        <li>
-          <Link to="reviews">Reviews</Link>
-        </li>
-      </ul>
-      <hr />
-      <Outlet />
-    </>
-  );
+  return <GalleryItem movie={movie} />;
 }
--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 
 import moviesApi from '../services/api-service';
 
+import styles from './Reviews.module.css';
+
 export default function Reviews() {
   const [reviews, setReviews] = useState([]);
   const { movieId } = useParams();
@@ -16,9 +18,9 @@ export default function Reviews() {
       {reviews.length !== 0 ? (
         <ul>
           {reviews.map(review => (
-            <li key={review.id}>
-              <p>{review.author}</p>
-              <p>{review.content}</p>
+            <li key={review.id} className={styles.Item}>
+              <h2 className={styles.Title}>Author: {review.author}</h2>
+              <p className={styles.Content}>{review.content}</p>
             </li>
           ))}
         </ul>
